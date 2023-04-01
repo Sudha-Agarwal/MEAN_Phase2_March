a3c2db82c1ddbc329b21af383a3ff086b709c87e
Sudha Agarwal <sudha.agarwal84@gmail.com>
2023-04-01 08:01:46 +0530

    added product list code

--- Angular/my-first-proj/src/app/product-mobile-list/product-mobile-list.component.ts
+++ Angular/my-first-proj/src/app/product-mobile-list/product-mobile-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { DataService } from '../data.service';
 import { Product } from '../product';
 
@@ -8,13 +9,17 @@ import { Product } from '../product';
   templateUrl: './product-mobile-list.component.html',
   styleUrls: ['./product-mobile-list.component.css']
 })
-export class ProductMobileListComponent implements OnInit{ 
+export class ProductMobileListComponent implements OnInit, OnDestroy{ 
   products:Product[];
+  sub:Subscription;
   constructor(route:ActivatedRoute, private ds:DataService){
   }
+  ngOnDestroy(): void {
+   this.sub.unsubscribe();
+  }
   ngOnInit(): void {
-    this.ds.getProducts('Mobile')
-    .subscribe(products => this.products=products)
+    this.sub = this.ds.getProducts('Mobile')
+    .subscribe(products => this.products=products);    
   }
 
   
